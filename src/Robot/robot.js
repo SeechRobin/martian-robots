08f9f709e603a77d28222045500dfc37617c8876
Robin Mukanganise <seechrobin@gmail.com>
2018-12-02 22:47:36 +0000

    added robot functionality, turn left right and move forward and tests

--- src/Robot/robot.js
+++ src/Robot/robot.js
@@ -1,3 +1,4 @@
+const DIRECTIONS = ["N", "E", "S", "W"];
 class Robot {
   /**
    * Create Robot with (x,y) coordinates, heading and the instructions
@@ -15,6 +16,55 @@ class Robot {
     }),
       (this.instructions = instructions),
       (this.lost = false);
+  }
+
+  /**
+   * Moves the robot forward if facing:
+   * N: y+1
+   * E: x+1
+   * S: y-1
+   * W: x-1
+   * return new advanced position
+   */
+  moveForward() {
+    switch (this.position.heading) {
+      case "N":
+        return (this.position = { ...this.position, y: this.position.y + 1 });
+      case "E":
+        return (this.position = { ...this.position, x: this.position.x + 1 });
+      case "S":
+        return (this.position = { ...this.position, y: this.position.y - 1 });
+      case "W":
+        return (this.position = { ...this.position, x: this.position.x - 1 });
+      default:
+        return this.position;
+    }
+  }
+  /**
+   * The robot turns left 90 degrees and remains on the current grid point.
+   * Check if current direction is in the begining of the array, if it is at the beginning make the
+   * last one the new direction. If not new direction is position of current direction -1
+   */
+  turnLeft() {
+    if (DIRECTIONS.indexOf(this.position.heading) === 0) {
+      this.position.heading = DIRECTIONS[3];
+    } else {
+      this.position.heading =
+        DIRECTIONS[DIRECTIONS.indexOf(this.position.heading) - 1];
+    }
+  }
+  /**
+   * The robot turns right 90 degrees and remains on the current grid point
+   * Check if current direction is in the end of the array, if it is at the end make the
+   * first item the new direction, if not new direction is position of current direction +1
+   */
+  turnRight() {
+    if (DIRECTIONS.indexOf(this.position.heading) === 3) {
+      this.position.heading = DIRECTIONS[0];
+    } else {
+      this.position.heading =
+        DIRECTIONS[DIRECTIONS.indexOf(this.position.heading) + 1];
+    }
   }
 }
 
--- src/test/robot.spec.js
+++ src/test/robot.spec.js
@@ -9,3 +9,36 @@ describe("Robot() ", function() {
     expect(robot).to.be.an.instanceof(Robot);
   });
 });
+
+describe("Robot movement ", function() {
+  it("Turning Left", function() {
+    robot = new Robot(1, 1, "N", []);
+    robot.turnLeft();
+    assert.equal(robot.position.heading, "W");
+  });
+  it("Turning Right", function() {
+    robot = new Robot(1, 1, "N", []);
+    robot.turnRight();
+    assert.equal(robot.position.heading, "E");
+  });
+  it("Move Forward Facing North", function() {
+    robot = new Robot(1, 1, "N", []);
+    robot.moveForward();
+    assert.equal(robot.position.y, 2);
+  });
+  it("Move Forward Facing East", function() {
+    robot = new Robot(1, 1, "E", []);
+    robot.moveForward();
+    assert.equal(robot.position.x, 2);
+  });
+  it("Move Forward Facing South", function() {
+    robot = new Robot(1, 1, "S", []);
+    robot.moveForward();
+    assert.equal(robot.position.y, 0);
+  });
+  it("Move Forward Facing West", function() {
+    robot = new Robot(1, 1, "W", []);
+    robot.moveForward();
+    assert.equal(robot.position.x, 0);
+  });
+});
